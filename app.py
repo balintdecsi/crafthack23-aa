af2107969b6a3f3e2e2091ed1c7d8b0b8db07c8a
balintdecsi <balint.decsi@gmail.com>
2023-05-19 00:59:59 +0200

    Minor enhances

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
+from dev.src.seon import get_user_score
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b5050c392097268742d148b8dfc7afb646a629a83d75c660'
@@ -18,10 +19,10 @@ def create():
 
         if not content:
             flash('Content is required!')
-        elif 'https' not in content:
-            flash('Content must have https in it!')
+        # elif 'https' not in content:
+        #     flash('Content must have https in it!')
         else:
-            messages.append({'content': content})
+            messages.append({'content': get_user_score(content)})
             return redirect(url_for('index'))
 
     return render_template('create.html')
--- dev/src/seon.py
+++ dev/src/seon.py
@@ -5,56 +5,57 @@ headers = {
   "X-API-KEY": "51e9a24b-53f5-42db-9935-cb24cd8f3a3f"
 }
 
-payload = {
-    "config": {
-        "ip": {
-          "include": "flags,history,id",
-          "version": "v1.1"
+def get_user_score(user_name: str) -> str:
+    payload = {
+        "config": {
+            "ip": {
+            "include": "flags,history,id",
+            "version": "v1.1"
+            },
+            "email": {
+            "include": "flags,history,id",
+            "version": "v2.2"
+            },
+            "phone": {
+            "include": "flags,history,id",
+            "version": "v1.4"
+            },
+            "ip_api": True,
+            "email_api": True,
+            "phone_api": True,
+            "device_fingerprinting": True,
         },
-        "email": {
-          "include": "flags,history,id",
-          "version": "v2.2"
-        },
-        "phone": {
-          "include": "flags,history,id",
-          "version": "v1.4"
-        },
-        "ip_api": True,
-        "email_api": True,
-        "phone_api": True,
-        "device_fingerprinting": True,
-    },
-    "ip": "",
-    "action_type": "account_register",
-    "affiliate_id": "",
-    "affiliate_name": "",
-    "email": "",
-    "email_domain": "",
-    "password_hash": "",
-    "user_fullname": "",
-    "user_name": "",
-    "user_id": "",
-    "transaction_id": "",
-    "user_dob": "",
-    "user_category": "",
-    "user_account_status": "",
-    "user_created": "",
-    "user_country": "",
-    "user_city": "",
-    "user_region": "",
-    "user_zip": "",
-    "user_street": "",
-    "user_street2": "",
-    "session": "",
-    "phone_number": "",
-    "transaction_type": "",
-    "bonus_campaign_id": "",
-    "merchant_id": "",
-    "details_url": "",
-    "custom_fields": {
+        "ip": "",
+        "action_type": "account_register",
+        "affiliate_id": "",
+        "affiliate_name": "",
+        "email": "",
+        "email_domain": "",
+        "password_hash": "",
+        "user_fullname": "",
+        "user_name": user_name,
+        "user_id": "",
+        "transaction_id": "",
+        "user_dob": "",
+        "user_category": "",
+        "user_account_status": "",
+        "user_created": "",
+        "user_country": "",
+        "user_city": "",
+        "user_region": "",
+        "user_zip": "",
+        "user_street": "",
+        "user_street2": "",
+        "session": "",
+        "phone_number": "",
+        "transaction_type": "",
+        "bonus_campaign_id": "",
+        "merchant_id": "",
+        "details_url": "",
+        "custom_fields": {
+        }
     }
-}
 
-response = requests.post('https://api.seon.io/SeonRestService/fraud-api/v2.0', headers=headers,
-                         data=json.dumps(payload))
-print(response.json())
+    response = requests.post('https://api.seon.io/SeonRestService/fraud-api/v2.0', headers=headers,
+                            data=json.dumps(payload))
+    return response.json()['data']['fraud_score']
