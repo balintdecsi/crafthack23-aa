1403e2636a7324a58409c7225c5f5b178bc2a914
balintdecsi <balint.decsi@gmail.com>
2023-05-18 23:34:15 +0200

    Tailor template code to fraud detection needs

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b5050c392097268742d148b8dfc7afb646a629a83d75c660'
 
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
+messages = [{'content': 'Message One Content'},
+            {'content': 'Message Two Content'}
             ]
 
 @app.route('/')
@@ -16,15 +14,14 @@ def index():
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
-        title = request.form['title']
         content = request.form['content']
 
-        if not title:
-            flash('Title is required!')
-        elif not content:
+        if not content:
             flash('Content is required!')
+        elif 'https' not in content:
+            flash('Content must have https in it!')
         else:
-            messages.append({'title': title, 'content': content})
+            messages.append({'content': content})
             return redirect(url_for('index'))
 
     return render_template('create.html')
