8b1ea1331129378f247dfac3aebc4b81ccf2ccb4
robertagocs <robert.agocs@outlook.com>
2023-05-19 10:54:32 +0200

    store api keys in env variables

--- dev/src/scoring.py
+++ dev/src/scoring.py
@@ -1,12 +1,15 @@
 from dev.src import originality_ai
 from dev.src import seon
+import os
 
 
-originality_api_key = 'i7h8mkczvloynfp364eq0a5ujrdxbt2g'
-seon_api_key = "51e9a24b-53f5-42db-9935-cb24cd8f3a3f"
+originality_api_key = os.environ.get('originality_api_key')
+seon_api_key = os.environ.get('seon_api_key')
 
+seon_score_trashold = 10
+ai_score_trashold = 0.1
 
-def make_review_score(originality_api_key, seon_api_key, URL):
+def make_review_score(URL, originality_api_key = originality_api_key, seon_api_key = seon_api_key):
     ai_score = originality_ai.get_originality_ai_score(api_key = originality_api_key, URL = URL)
     seon_score = seon.get_seon_fraud_score(api_key = seon_api_key,  URL = URL)
        
