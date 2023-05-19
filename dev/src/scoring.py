600c7ba4ee6bf413f85348922e922043e7ba14ad
balintdecsi <balint.decsi@gmail.com>
2023-05-19 11:10:39 +0200

    Add security for keys

--- dev/src/scoring.py
+++ dev/src/scoring.py
@@ -1,10 +1,9 @@
 from dev.src import originality_ai
 from dev.src import seon
+import os
 
-
-originality_api_key = 'i7h8mkczvloynfp364eq0a5ujrdxbt2g'
-seon_api_key = "51e9a24b-53f5-42db-9935-cb24cd8f3a3f"
-
+originality_api_key = os.environ.get('originality_api_key')
+seon_api_key = os.environ.get('seon_api_key')
 
 def make_review_score(originality_api_key, seon_api_key, URL):
     ai_score = originality_ai.get_originality_ai_score(api_key = originality_api_key, URL = URL)
