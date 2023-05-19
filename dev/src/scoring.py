e28870c0c27fadc4bc2faf75e28466d3c38f15f6
robertagocs <robert.agocs@outlook.com>
2023-05-19 10:58:39 +0200

    api storeage in env variable

--- dev/src/scoring.py
+++ dev/src/scoring.py
@@ -9,7 +9,7 @@ seon_api_key = os.environ.get('seon_api_key')
 seon_score_trashold = 10
 ai_score_trashold = 0.1
 
-def make_review_score(URL, originality_api_key = originality_api_key, seon_api_key = seon_api_key):
+def make_review_score(originality_api_key = originality_api_key, seon_api_key = seon_api_key, URL = ''):
     ai_score = originality_ai.get_originality_ai_score(api_key = originality_api_key, URL = URL)
     seon_score = seon.get_seon_fraud_score(api_key = seon_api_key,  URL = URL)
        
