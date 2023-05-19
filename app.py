d068700c8ea2389819354010cce70050738ab249
balintdecsi <balint.decsi@gmail.com>
2023-05-19 06:15:48 +0200

    Reduce displayed messages to one

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from dev.src.seon import get_user_score
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b5050c392097268742d148b8dfc7afb646a629a83d75c660'
 
-messages = [{'content': 'Message One Content'},
-            {'content': 'Message Two Content'}
-            ]
+messages = [{'content': 'Message One Content'}]
 
 @app.route('/')
 def index():
@@ -22,7 +20,7 @@ def create():
         # elif 'https' not in content:
         #     flash('Content must have https in it!')
         else:
-            messages.append({'content': get_user_score(content)})
+            messages[0] = {'content': 'score for this-1, 1]}
             return redirect(url_for('index'))
 
     return render_template('create.html')
