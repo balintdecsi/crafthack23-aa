f8bc8e0fb25f3bf915df72444d254b1492b2c4c2
robertagocs <robert.agocs@outlook.com>
2023-05-19 11:32:43 +0200

    score treshold variables added

--- dev/src/scoring.py
+++ dev/src/scoring.py
@@ -6,14 +6,14 @@ import os
 originality_api_key = os.environ.get('originality_api_key')
 seon_api_key = os.environ.get('seon_api_key')
 
-seon_score_trashold = 10
-ai_score_trashold = 0.1
+seon_score_treshold = 10
+ai_score_treshold = 0.1
 
 def make_review_score(originality_api_key, seon_api_key, URL):
     ai_score = originality_ai.get_originality_ai_score(api_key = originality_api_key, URL = URL)
     seon_score = seon.get_seon_fraud_score(api_key = seon_api_key,  URL = URL)
        
-    if seon_score >= 10 or ai_score >= 0.1:
+    if seon_score >= seon_score_treshold or ai_score >= ai_score_treshold:
         return 1
     else:
         return 0
